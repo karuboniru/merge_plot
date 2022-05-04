0bce6ef2538be3a2707608e6d7344998b2819038
Qiyu Yan <yanqiyu@fedoraproject.org>
2022-05-05 00:44:56 +0800

    - use TH1 - Draw eps file - enable rebin

--- main.cxx
+++ main.cxx
@@ -1,6 +1,6 @@
 #include <TCanvas.h>
 #include <TFile.h>
-#include <TH1D.h>
+#include <TH1.h>
 #include <TLegend.h>
 #include <TStyle.h>
 #include <TSystem.h>
@@ -23,8 +23,8 @@ int main(int argc, char const *argv[])
     }
     std::string plot_title{};
     // std::vector<std::string> legends{}, plot_names{};
-    // std::vector<std::unique_ptr<TH1D>> histos{};
-    using entry_t = std::pair<std::string, std::unique_ptr<TH1D>>;
+    // std::vector<std::unique_ptr<TH1>> histos{};
+    using entry_t = std::pair<std::string, std::unique_ptr<TH1>>;
     std::string output_prefix = argv[2];
     double max{};
     std::vector<entry_t> entries{};
@@ -86,22 +86,28 @@ int main(int argc, char const *argv[])
             auto &file_path = input[1];
             auto &plot_name = input[2];
             double scale = 1.;
+            size_t rebin_factor = 1;
             if (input.size() > 3)
                 scale = std::stod(input[3]);
+            if (input.size() > 4)
+                rebin_factor = std::stoul(input[4]);
             TFile file{file_path.c_str(), "READ"};
             if (!file.IsOpen())
             {
                 std::cout << "Error: Could not open file " << file_path << std::endl;
                 return 1;
             }
-            std::unique_ptr<TH1D> hist{dynamic_cast<TH1D *>(file.Get(plot_name.c_str()))};
+            std::unique_ptr<TH1> hist{dynamic_cast<TH1 *>(file.Get(plot_name.c_str()))};
             if (!hist)
             {
                 std::cout << "Error: Could not get histogram " << plot_name << " from file " << file_path << std::endl;
                 return 1;
             }
-            hist->Scale(scale);
+            hist->Scale(scale / rebin_factor);
+            if (rebin_factor != 1)
+                hist = std::unique_ptr<TH1>{dynamic_cast<TH1 *>(hist->Rebin(rebin_factor)->Clone())};
             hist->GetXaxis()->SetRangeUser(0, uplimit);
+            std::cout << "plotting " << plot_name << " from file " << file_path << " scale " << scale << " rebin with " << rebin_factor << std::endl;
             max = std::max(max, hist->GetMaximum());
             entries.emplace_back(legend, std::move(hist));
             file.Close();
@@ -110,7 +116,7 @@ int main(int argc, char const *argv[])
             std::array<int, 10> col{kRed, kGreen, kBlue, kMagenta, kCyan,
                                     kOrange, kViolet, kGray, kYellow, kBlack};
             auto canvas = getCanvas();
-            auto leg = std::make_unique<TLegend>();
+            auto leg = std::make_unique<TLegend>(.7, .7, .9, .9);
             for (std::size_t i = 0; i < entries.size(); ++i)
             {
                 auto &[legend_title, hist] = entries[i];
@@ -118,14 +124,16 @@ int main(int argc, char const *argv[])
                 hist->SetLineColor(col[i]);
                 hist->SetLineWidth(1);
                 leg->AddEntry(hist.get(), legend_title.c_str(), "l");
-                auto opt = i == 0 ? "hist" : "hist same";
+                auto opt = i == 0 ? "hist C" : "hist C same";
                 hist->SetTitle(plot_title.c_str());
                 hist->SetMaximum(max * 1.1);
+                hist->SetMinimum(0);
                 hist->Draw(opt);
             }
             leg->Draw();
             canvas->SaveAs((output_prefix + ".pdf").c_str());
             canvas->SaveAs((output_prefix + ".png").c_str());
+            canvas->SaveAs((output_prefix + ".eps").c_str());
         }
     }
     return 0;
